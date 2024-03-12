852f70bc23b1a43977f135a0e61032c46804a258
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-03-12 23:42:52 +0530

    Update tts.py

--- wbb/modules/tts.py
+++ wbb/modules/tts.py
@@ -1,42 +1,42 @@
-import traceback
-from asyncio import get_running_loop
-from io import BytesIO
+import traceback 
+from asyncio import get_running_loop 
+from io import BytesIO 
 
-from googletrans import Translator
-from gtts import gTTS
-from pyrogram import filters
-from pyrogram.types import Message
+from googletrans import Translator 
+from gtts import gTTS 
+from pyrogram import filters 
+from pyrogram.types import Message 
 
-from wbb import app
-
-
-def convert(text):
-    audio = BytesIO()
-    i = Translator().translate(text, dest="en")
-    lang = i.src
-    tts = gTTS(text, lang=lang)
-    audio.name = lang + ".mp3"
-    tts.write_to_fp(audio)
-    return audio
+from wbb import app 
 
 
-@app.on_message(filters.command("tts"))
-async def text_to_speech(_, message: Message):
-    if not message.reply_to_message:
-        return await message.reply_text("Reply to some text ffs.")
-    if not message.reply_to_message.text:
-        return await message.reply_text("Reply to some text ffs.")
-    m = await message.reply_text("Processing")
-    text = message.reply_to_message.text
-    sender_mention = message.reply_to_message.from_user.mention
-    try:
-        loop = get_running_loop()
-        audio = await loop.run_in_executor(None, convert, text)
-        caption = f"Original message by {sender_mention}"
-        await message.reply_audio(audio, caption=caption)
-        await m.delete()
-        audio.close()
-    except Exception as e:
-        await m.edit(str(e))
-        e = traceback.format_exc()
+def convert(text): 
+    audio = BytesIO() 
+    i = Translator().translate(text, dest="en") 
+    lang = i.src 
+    tts = gTTS(text, lang=lang) 
+    audio.name = lang + ".mp3" 
+    tts.write_to_fp(audio) 
+    return audio 
+
+
+@app.on_message(filters.command("tts")) 
+async def text_to_speech(_, message: Message): 
+    if not message.reply_to_message: 
+        return await message.reply_text("Reply to some text ffs.") 
+    if not message.reply_to_message.text: 
+        return await message.reply_text("Reply to some text ffs.") 
+    m = await message.reply_text("Processing") 
+    text = message.reply_to_message.text 
+    sender_mention = message.reply_to_message.from_user.mention 
+    try: 
+        loop = get_running_loop() 
+        audio = await loop.run_in_executor(None, convert, text) 
+        caption = f"Original message by {sender_mention}: [Link to Message]({message.link})"
+        await message.reply_audio(audio, caption=caption) 
+        await m.delete() 
+        audio.close() 
+    except Exception as e: 
+        await m.edit(str(e)) 
+        e = traceback.format_exc() 
         print(e)
