04d1090b5752f363bb13790eabd196b297ce1fb6
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-03-12 10:24:18 +0530

    Update tts.py

--- wbb/modules/tts.py
+++ wbb/modules/tts.py
@@ -28,14 +28,15 @@ async def text_to_speech(_, message: Message):
         return await message.reply_text("Reply to some text ffs.")
     m = await message.reply_text("Processing")
     text = message.reply_to_message.text
+    sender_mention = message.reply_to_message.from_user.mention
     try:
         loop = get_running_loop()
         audio = await loop.run_in_executor(None, convert, text)
-        await message.reply_audio(audio, caption="Text to Speech")
+        caption = f"Text to Speech\n\nOriginal message by {sender_mention}"
+        await message.reply_audio(audio, caption=caption)
         await m.delete()
         audio.close()
     except Exception as e:
         await m.edit(str(e))
         e = traceback.format_exc()
         print(e)
-
