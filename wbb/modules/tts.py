57515aedf557d5f33ea4c7077bb961ccbf622123
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-03-12 10:43:09 +0530

    Update tts.py

--- wbb/modules/tts.py
+++ wbb/modules/tts.py
@@ -32,7 +32,7 @@ async def text_to_speech(_, message: Message):
     try:
         loop = get_running_loop()
         audio = await loop.run_in_executor(None, convert, text)
-        caption = f"Text to Speech\n\nOriginal message by {sender_mention}"
+        caption = f"Original message by {sender_mention}"
         await message.reply_audio(audio, caption=caption)
         await m.delete()
         audio.close()
