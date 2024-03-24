c92533db29962da914c4faeefebd4d7dcbd22589
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-03-24 15:18:04 +0530

    Update music.py

--- wbb/modules/music.py
+++ wbb/modules/music.py
@@ -3,79 +3,73 @@ import os
 from asyncio import get_running_loop  
 from functools import partial  
 from io import BytesIO  
-
+  
 from pyrogram import filters  
 from pytube import YouTube  
 from requests import get  
-
+  
 from wbb import aiohttpsession as session  
 from wbb import app, arq  
 from wbb.core.decorators.errors import capture_err  
 from wbb.utils.pastebin import paste  
-
+  
 __MODULE__ = "Music"  
 __HELP__ = """  
 /ytmusic [link] To Download Music From Various Websites Including Youtube. [SUDOERS]  
 /saavn [query] To Download Music From Saavn.  
 /lyrics [query] To Get Lyrics Of A Song.  
-/vsong [link] To Download Video From YouTube. [SUDOERS]  
 """  
-
+  
 is_downloading = False  
-
-
+  
+  
 def download_youtube_audio(arq_resp):  
     r = arq_resp.result[0]  
-
+  
     title = r.title  
     performer = r.channel  
-
+  
     m, s = r.duration.split(":")  
     duration = int(datetime.timedelta(minutes=int(m), seconds=int(s)).total_seconds())  
-
+  
     if duration > 1800:  
         return  
-
+  
     thumb = get(r.thumbnails[0]).content  
     with open("thumbnail.png", "wb") as f:  
         f.write(thumb)  
     thumbnail_file = "thumbnail.png"  
-
+  
     url = f"https://youtube.com{r.url_suffix}"  
     yt = YouTube(url)  
     audio = yt.streams.filter(only_audio=True).get_audio_only()  
-
+  
     out_file = audio.download()  
     base, _ = os.path.splitext(out_file)  
     audio_file = base + ".mp3"  
     os.rename(out_file, audio_file)  
-
+  
     return [title, performer, duration, audio_file, thumbnail_file]  
-
-
-def download_youtube_video(url):
-    yt = YouTube(url)
-    video = yt.streams.get_highest_resolution()
-    out_file = video.download()
-    return out_file
-
-
+  
+  
 @app.on_message(filters.command(["ytmusic", "song"]))  
 @capture_err  
 async def music(_, message):  
     global is_downloading  
     if len(message.command) < 2:  
         return await message.reply_text("/ytmusic needs a query as argument")  
-
+  
     url = message.text.split(None, 1)[1]  
     if is_downloading:  
         return await message.reply_text(  
             "Another download is in progress, try again after sometime."  
         )  
-    is_downloading = False 
-
-    user_name = message.from_user.first_name if message.from_user else "Unknown User" 
-
+    is_downloading = False  
+  
+    # Get the name of the user who requested the music 
+    user_name = message.from_user.first_name if message.from_user else "Unknown User"  
+  
+    # Send a message indicating the start of the download process with the user's name 
     m = await message.reply_text(  
         f"{user_name.capitalize()} is downloading {url}", disable_web_page_preview=True  
     )  
@@ -85,7 +79,7 @@ async def music(_, message):
         music = await loop.run_in_executor(  
             None, partial(download_youtube_audio, arq_resp)  
         )  
-
+  
         if not music:  
             return await message.reply_text("[ERROR]: MUSIC TOO LONG")  
         (  
@@ -98,56 +92,97 @@ async def music(_, message):
     except Exception as e:  
         is_downloading = False  
         return await m.edit(str(e))  
-
+  
+    # Mention the user who requested the music and send the audio file 
     await message.reply_audio(  
         audio_file,  
         duration=duration,  
         performer=performer,  
         title=title,  
         thumb=thumbnail_file,  
-        caption=f"{user_name.capitalize()}, here is your requested music."  
+        caption=f"{user_name.capitalize()}, here is your requested music."  # Mention the user with capitalized name 
     )  
-
+  
+    # Delete the command message 
     await message.delete()  
+  
     await m.delete()  
     os.remove(audio_file)  
     os.remove(thumbnail_file)  
     is_downloading = False  
-
-
-@app.on_message(filters.command(["vsong", "video"]))  
-@capture_err  
-async def video(_, message):  
-    global is_downloading  
-    if len(message.command) < 2:  
-        return await message.reply_text("/vsong needs a YouTube link as argument")  
-
-    url = message.text.split(None, 1)[1]  
-    if is_downloading:  
-        return await message.reply_text(  
-            "Another download is in progress, try again after sometime."  
-        )  
+  
+  
+# Funtion To Download Song  
+async def download_song(url):  
+    async with session.get(url) as resp:  
+        song = await resp.read()  
+    song = BytesIO(song)  
+    song.name = "a.mp3"  
+    return song 
+  
+  
+@app.on_message(filters.command("saavn")) 
+@capture_err 
+async def jssong(_, message): 
+    global is_downloading 
+    if len(message.command) < 2: 
+        return await message.reply_text("/saavn requires an argument.") 
+    if is_downloading: 
+        return await message.reply_text( 
+            "Another download is in progress, try again after sometime." 
+        ) 
+    is_downloading = True 
+    text = message.text.split(None, 1)[1] 
+    m = await message.reply_text("Searching...") 
+    try: 
+        songs = await arq.saavn(text) 
+        if not songs.ok: 
+            await m.edit(songs.result) 
+            is_downloading = False 
+            return 
+        sname = songs.result[0].song 
+        slink = songs.result[0].media_url 
+        ssingers = songs.result[0].singers 
+        sduration = songs.result[0].duration 
+        await m.edit("Downloading") 
+        song = await download_song(slink) 
+        await m.edit("Uploading") 
+        await message.reply_audio( 
+            audio=song, 
+            title=sname, 
+            performer=ssingers, 
+            duration=sduration, 
+        ) 
+        await m.delete() 
+    except Exception as e: 
+        is_downloading = False 
+        return await m.edit(str(e)) 
     is_downloading = False 
-
-    user_name = message.from_user.first_name if message.from_user else "Unknown User" 
-
-    m = await message.reply_text(  
-        f"{user_name.capitalize()} is downloading {url}", disable_web_page_preview=True  
-    )  
-    try:  
-        video_file = await download_youtube_video(url)
-    except Exception as e:  
-        is_downloading = False  
-        return await m.edit(str(e))  
-
-    await message.reply_video(  
-        video_file,  
-        caption=f"{user_name.capitalize()}, here is your requested video."  
-    )  
-
-    await message.delete()  
-    await m.delete()  
-    os.remove(video_file)  
-    is_downloading = False  
-
-# The rest of the code remains unchanged...
+    song.close() 
+  
+  
+# Lyrics 
+  
+  
+@app.on_message(filters.command("lyrics")) 
+async def lyrics_func(_, message): 
+    if len(message.command) < 2: 
+        return await message.reply_text("**Usage:**\n/lyrics [QUERY]") 
+    m = await message.reply_text("**Searching**") 
+    query = message.text.strip().split(None, 1)[1] 
+  
+    resp = await arq.lyrics(query) 
+  
+    if not (resp.ok and resp.result): 
+        return await m.edit("No lyrics found.") 
+  
+    song = resp.result[0] 
+    song_name = song["song"] 
+    artist = song["artist"] 
+    lyrics = song["lyrics"] 
+    msg = f"**{song_name}** | **{artist}**\n\n__{lyrics}__" 
+  
+    if len(msg) > 4095: 
+        msg = await paste(msg) 
+        msg = f"**LYRICS_TOO_LONG:** [URL]({msg})" 
+    return await m.edit(msg)
